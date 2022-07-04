0ea62b3917b26297491389a03f5051564682d6af
Harsh Bharath N <harshbharath.n@gmail.com>
2022-07-03 20:39:09 -0400

    snake_game_directions_movement

--- Day_20/snake_game.py
+++ Day_20/snake_game.py
@@ -2,12 +2,20 @@ from turtle import Turtle, Screen
 import random, time
 
 
-def turn_left(tut):
-    return tut.left(90)
+def turn_up():
+    return tuts[0].setheading(90)
 
 
-def turn_right(tut):
-    return tut.right(90)
+def turn_down():
+    return tuts[0].setheading(270)
+
+
+def turn_left():
+    return tuts[0].setheading(180)
+
+
+def turn_right():
+    return tuts[0].setheading(0)
 
 
 def game():
@@ -16,20 +24,23 @@ def game():
     tut2.color('red')
     tut2.pu()
     tut2.setposition(tut1.xcor() - 20, tut1.ycor())
+    global tuts
     tuts = [tut1, tut2]
     sc.update()
-    sc.listen()
-    i=0
-    while i<10:
+
+    while True:
         sc.update()
         time.sleep(.1)
         for _ in range(len(tuts)-1, 0, -1):
             tuts[_].pu()
             tuts[_].setposition(tuts[_+-1].xcor(), tuts[_-1].ycor())
         tuts[0].fd(20)
-        sc.onkeypress(key="Up", fun=turn_left)
+        sc.onkeypress(key="Up", fun=turn_up)
         sc.onkeypress(key='Right', fun=turn_right)
-        i+=1
+        sc.onkeypress(key="Left", fun=turn_left)
+        sc.onkeypress(key='Down', fun=turn_down)
+
+
 
 
 
@@ -38,6 +49,8 @@ sc = Screen()
 sc.bgcolor('gray')
 sc.setup(width=600, height=600)
 sc.title('-SNAKE GAME-')
+sc.listen()
+
 
 sc.tracer(0)
 game()

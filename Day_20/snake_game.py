5d598b365ac79153f7b4332133982fb715912504
Harsh Bharath N <harshbharath.n@gmail.com>
2022-07-04 21:28:42 -0400

    snake_game_random_food

--- Day_20/snake_game.py
+++ Day_20/snake_game.py
@@ -18,6 +18,43 @@ def turn_right():
     return tuts[0].setheading(0)
 
 
+def food():
+    global snack
+    snack = Turtle(shape='circle')
+    snack.pu()
+    snack.shapesize(stretch_len=.5, stretch_wid=.5)
+    snack.color('blue')
+    snack.speed('fastest')
+    snack.setposition(random.randint(-280, 200), random.randint(-280, 200))
+    return snack.xcor()
+
+
+def show_score(count):
+    score.reset()
+    score.hideturtle()
+    score.pu()
+    score.hideturtle()
+    score.setposition(0, 280)
+    score.write(arg=f"Score: {count}", move=True, align='center', font=('Arial', 15, 'normal'))
+
+
+
+def eat():
+    global count
+    if tuts[0].distance(snack) < 15:
+        count += 1
+        snack.hideturtle()
+        show_score(count)
+        food()
+        return False
+    elif tuts[0].xcor() >= 295:
+        print('inside')
+        return False
+
+def del_food():
+    return False
+
+
 def game():
     tut1 = Turtle(shape='square')
     tut2 = Turtle(shape='square')
@@ -39,10 +76,18 @@ def game():
         sc.onkeypress(key='Right', fun=turn_right)
         sc.onkeypress(key="Left", fun=turn_left)
         sc.onkeypress(key='Down', fun=turn_down)
+        if not eat():
+            return True
 
 
 
+food()
+global score
+score = Turtle()
+score.hideturtle()
 
+global count
+count = 0
 
 
 sc = Screen()
@@ -53,6 +98,9 @@ sc.listen()
 
 
 sc.tracer(0)
-game()
+count = 0
+if game():
+    sc.textinput(title="Score", prompt=f"you lost. Your score is {count}")
+
 
 sc.exitonclick()

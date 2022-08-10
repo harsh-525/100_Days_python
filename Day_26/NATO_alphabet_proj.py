5a35393efb65680388598f59a679b833abd1c61e
Harsh Bharath N <harshbharath.n@gmail.com>
2022-08-09 21:17:08 -0400

    NATO_with_exception_handling

--- Day_26/NATO_alphabet_proj.py
+++ Day_26/NATO_alphabet_proj.py
@@ -4,9 +4,20 @@ data = pd.read_csv('NATO-alphabet-start/nato_phonetic_alphabet.csv')
 
 # TODO 1: Create a dictionary of NATO alphabets
 nato_dict = {rows.letter: rows.code for (index, rows) in data.iterrows()}
+
+
 # print(nato_dict)
 
-# TODO 2: Create a list of NATO words from User input
-user_input = input("Type a word containing only letters: ")
-result = [nato_dict[_.upper()] for _ in user_input]
-print(result)
+def generate_phonetic():
+    # TODO 2: Create a list of NATO words from User input
+    user_input = input("Type a word containing only letters: ")
+    try:
+        result = [nato_dict[_.upper()] for _ in user_input]
+    except KeyError as msg:
+        print(f"Missing key={msg}")
+        generate_phonetic()
+    else:
+        print(result)
+
+
+generate_phonetic()
